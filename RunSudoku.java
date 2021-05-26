8a66771b0d9b8e8506e76be672f2076174b9989d
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2021-05-26 11:55:43 -0400

    Add Docs

--- RunSudoku.java
+++ RunSudoku.java
@@ -1,3 +1,7 @@
+/**
+  * Description: The class which creates an instance of the gui-- call java RunSudoku.java to run the applet
+  */
+
 class RunSudoku {
 
   public static void main(String[] args){
