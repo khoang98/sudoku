d2a5df00ac94d60219296fc5bbf7f26b262e4790
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2021-05-27 11:54:37 -0400

    Add avg

--- SudokuTester.java
+++ SudokuTester.java
@@ -3,18 +3,25 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-
+/**
+  * Description: A class which handles the instanciation, execution, and gathering of results for each of the tests
+  *@param: emb: test embarrasingly parallel solver?
+  *@param: prolog: test prolog solver?
+  *@param: par: test recursive parralell solver?
+  *param: test fork join pool sovler?
+  */
 public class SudokuTester {
 
     public static HashMap<String,Result> test(boolean emb, boolean prolog,boolean par, boolean fjp){
-        
+
         int[][] puzzle = genGrid(9);
 
         HashMap<String,Result> results = new HashMap<String,Result>();
         results.put("Baseline",testBaseline(copyGrid(puzzle)));
-        
+
         if(prolog){
           results.put("Prolog",testProlog(copyGrid(puzzle)));
         }
@@ -38,7 +45,7 @@ public class SudokuTester {
       for (int i = 0; i < 7; i++){
         randomNum = ThreadLocalRandom.current().nextInt(0, size*size);
         randVal = ThreadLocalRandom.current().nextInt(1, size);
-        
+
 
         if( new Sudoku(toReturn).isValid(randVal, ((randomNum/9)), randomNum%9)){
           toReturn[(randomNum/9)][randomNum%9]= randVal;
@@ -77,16 +84,16 @@ public class SudokuTester {
               Scanner scanner = new Scanner(f);
               prologArray= scanner.next();
               scanner.close();
-            
+
               int elems =0;
               for (char c :prologArray.toCharArray()){
                 if(c != ']' & c!= '[' & c!= ','){
                   pArray[elems/9][elems%9]= Character.getNumericValue(c);
                 }
-              } 
+              }
             } catch (FileNotFoundException e) {
               e.printStackTrace();
-            } 
+            }
           } catch (Exception e) {
               System.out.println("ERROR IN PROLOG CODE");
           }
@@ -122,5 +129,58 @@ public class SudokuTester {
 
     }
 
-}
+    public static HashMap<String,Result> avg(List<HashMap<String,Result>> listOfMaps){
+      int bline=0;
+      int pro=0;
+      int emb=0;
+      int par=0;
+      int fjp=0;
 
+      int s = listOfMaps.size();
+      System.out.println(s);
+
+
+     
+   
+
+      for(int i =0; i < s; i++){
+        HashMap<String,Result> r = listOfMaps.get(i);
+          if(r.containsKey("Baseline")){
+            bline = bline + Integer.valueOf(r.get("Baseline").time);
+          }
+          if(r.containsKey("Prolog")){
+            pro = pro + Integer.valueOf(r.get("Prolog").time);
+          }
+          if(r.containsKey("emb")){
+            emb = emb + Integer.valueOf(r.get("emb").time);
+          }
+          if(r.containsKey("Recursive")){
+            par = par + Integer.valueOf(r.get("Recursive").time);
+          }
+          if(r.containsKey("fjp")){
+            fjp = fjp + Integer.valueOf(r.get("fjp").time);
+          }
+      }
+      HashMap<String,Result> avgs = new HashMap<String,Result>();
+
+
+      if(listOfMaps.get(0).containsKey("Baseline")){
+        avgs.put("Baseline",new Result(Long.valueOf(bline/s),listOfMaps.get(0).get("Baseline").array));
+      }
+      if(listOfMaps.get(0).containsKey("Prolog")){
+        avgs.put("Prolog", new Result(Long.valueOf(pro/s),listOfMaps.get(0).get("Baseline").array));
+      }
+      if(listOfMaps.get(0).containsKey("emb")){
+        avgs.put("emb", new Result(Long.valueOf(emb/s),listOfMaps.get(0).get("emb").array));
+      }
+      if(listOfMaps.get(0).containsKey("Recursive")){
+        avgs.put("Recursive", new Result(Long.valueOf(par/s),listOfMaps.get(0).get("Recursive").array));
+      }
+      if(listOfMaps.get(0).containsKey("fjp")){
+        avgs.put("fjp", new Result(Long.valueOf(fjp/s),listOfMaps.get(0).get("fjp").array));
+      }
+      return avgs;
+    }
+   
+
+}
